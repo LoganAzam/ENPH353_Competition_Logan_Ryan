c102bd1c57861da2764bcab37ddbf3edc6d27edc
Ryan <ryanoishi@shaw.ca>
2026-03-31 15:49:24 -0700

    Added image saving to plate_sweep

--- src/competition_controller/node/plate_sweep.py
+++ src/competition_controller/node/plate_sweep.py
@@ -5,18 +5,37 @@ from std_msgs.msg import Int32
 from sensor_msgs.msg import Image
 
 PLATE_SWEEP_NUMBER = 0
+
+last_img = None
+last_state = 1
 images_list = []
 
-def callback_state(data):
-    if data.data == PLATE_SWEEP_NUMBER:
-        # Start subscribing to camera topic.
-        rospy.Subscriber('/rrbot/camera1/image_raw', Image, callback_img)
+# Needs to be global
+pub_state = None
 
-def plate_sweep_listener():
-    # Listen to state changer topic
+def callback_img(data):
+    global last_img
+    last_img = data
+
+def callback_state(data):
+    global pub_state
+    global last_state
+    if data.data == PLATE_SWEEP_NUMBER:
+        if last_img is not None:
+            images_list.append(last_img)
+        pub_state.publish(last_state)
+    else:
+        last_state = data.data
+
+def main():
+    global pub_state
     rospy.init_node('plate_sweep_node', anonymous=True)
+
+    # Initialize publishers and subscribers
+    pub_state = rospy.Publisher('/state_changer', Int32, queue_size=1, latch=True)
+    rospy.Subscriber('/B1/pi_camera/image_raw', Image, callback_img, queue_size=1)
     rospy.Subscriber('/state_changer', Int32, callback_state)
     rospy.spin()
 
 if __name__ == '__main__':
-    plate_sweep_listener()
+    main()
