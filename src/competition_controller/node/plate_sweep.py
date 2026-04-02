a18ce85be967271c36c54bfb4a281efa847f288e
Ryan <ryanoishi@shaw.ca>
2026-04-01 21:41:10 -0700

    Added saving images

--- src/competition_controller/node/plate_sweep.py
+++ src/competition_controller/node/plate_sweep.py
@@ -1,28 +1,42 @@
 #! /usr/bin/env python3
 
 import rospy
+import cv2
+from cv_bridge import CvBridge
 from std_msgs.msg import Int32
 from sensor_msgs.msg import Image
+import os
 
 PLATE_SWEEP_NUMBER = 0
 
-last_img = None
 last_state = 1
+last_img = None
 images_list = []
+bridge = CvBridge()
 
 # Needs to be global
 pub_state = None
 
 def callback_img(data):
     global last_img
-    last_img = data
+    last_img = bridge.imgmsg_to_cv2(data, "bgr8")
 
 def callback_state(data):
     global pub_state
     global last_state
     if data.data == PLATE_SWEEP_NUMBER:
+        print("Sweep state entered") # DEBUG
         if last_img is not None:
-            images_list.append(last_img)
+            images_list.append(last_img.copy())
+
+            # Save Image
+            if not os.path.exists("saved_images"):
+                os.makedirs("saved_images")
+            count = len(images_list)  # sequential number
+            filename = os.path.join("saved_images", f"image_{count:03d}.png")
+            cv2.imwrite(filename, last_img.copy())
+            print("IMG Saved") # DEBUG
+
         pub_state.publish(last_state)
     else:
         last_state = data.data
@@ -33,7 +47,7 @@ def main():
 
     # Initialize publishers and subscribers
     pub_state = rospy.Publisher('/state_changer', Int32, queue_size=1, latch=True)
-    rospy.Subscriber('/B1/pi_camera/image_raw', Image, callback_img, queue_size=1)
+    rospy.Subscriber('/B1/rrbot/camera1/image_raw', Image, callback_img, queue_size=1)
     rospy.Subscriber('/state_changer', Int32, callback_state)
     rospy.spin()
 
